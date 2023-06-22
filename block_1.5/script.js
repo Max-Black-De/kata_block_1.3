c28d23f416ecdf670d72b2fe92cb1f664668f63c
Black <black_de@yahoo.com>
2023-06-22 14:33:54 +0500

    created block 1.5 of webcore realized mobile vers

---
+++ block_1.5/script.js
@@ -0,0 +1,15 @@
+// init Swiper:
+new Swiper('.swiper', {
+    navigation: {
+        nextEl: '.swiper-button-next', 
+        prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: false,
+    },
+    autoHeight: false,
+    slidesPerView: 1.25,
+    centeredSlides: false,
+})
